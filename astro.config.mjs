5b44fb912071a53f658ed33a0fedfac8bf7426ad
Leith Inoubli <inoublileith6@gmail.com>
2026-02-06 19:03:53 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // filepath: /Users/normanleyvagarcia/Desktop/Astro/barcelonatango/astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
